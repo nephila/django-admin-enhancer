a0d5ccb48836da617c465a3410aeb94cb3f1dd95
Iacopo Spalletti <i.spalletti@nephila.it>
2012-09-29 10:18:37 +0200

    Added basic support for django-cms and django-filer

---
+++ admin_enhancer/support/djangocms.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from django.contrib.admin.sites import NotRegistered, AlreadyRegistered
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import admin
+from ..admin import EnhancedModelAdminMixin
+
+try:
+    from cms.admin.pageadmin import PageAdmin,Page
+
+    class EnhancedPageAdmin(EnhancedModelAdminMixin,PageAdmin):
+        pass
+
+    try:
+        admin.site.unregister(Page)
+    except NotRegistered, e:
+        print e
+    try:
+        admin.site.register(Page, EnhancedPageAdmin)
+    except AlreadyRegistered, e:
+        print e
+
+except ImportError:
+    print "django-cms not installed, patching skipped"
